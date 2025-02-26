ac3a94c6295bb0cecae5dc29d5437b2cfb38e4f2
rriverod <rriverod@chicagobooth.edu>
2025-02-26 20:25:52 +0000

    delete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -29,4 +29,14 @@ class MoviesController < ApplicationController
     redirect_to("/movies")
   end
 
+  def delete
+    the_id=params.fetch("an_id")
+    matching_records = Movie.where({:id=>the_id})
+    the_movie = matching_records.at(0)
+    the_movie.destroy
+    redirect_to("/movies")
+
+
+  end
+
 end
