bbc9e64296469f3100da04c5304930824b622575
rriverod <rriverod@chicagobooth.edu>
2025-02-26 23:23:21 +0000

    add an edit form in movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,8 +16,6 @@ class MoviesController < ApplicationController
   end
 
   def create
-    #{"the_title"=>"12", "the_year"=>"2", "the_duration"=>"3", "the_description"=>"1", "the_image"=>"1", "the_director_id"=>"1"}
-
     m=Movie.new
     m.title=params.fetch("the_title")
     m.year=params.fetch("the_year")
@@ -35,8 +33,21 @@ class MoviesController < ApplicationController
     the_movie = matching_records.at(0)
     the_movie.destroy
     redirect_to("/movies")
+  end
 
-
+  def update
+    m_id=params.fetch("the_id")
+    matching_records=Movie.where({:id=> m_id})
+    the_movie=matching_records.at(0)
+    
+    the_movie.title=params.fetch("the_title")
+    the_movie.year=params.fetch("the_year")
+    the_movie.duration=params.fetch("the_duration")
+    the_movie.description=params.fetch("the_description")
+    the_movie.image=params.fetch("the_image")
+    the_movie.director_id=params.fetch("the_director_id")
+    the_movie.save
+    redirect_to("/movies/#{the_movie.id}")
   end
 
 end
